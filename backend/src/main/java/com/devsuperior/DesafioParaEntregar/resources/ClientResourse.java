93c0d0ed91ba776326be3268e7652df899c89ddc
Danilo da Silva Souza <danilosilva441@gmail.com>
2021-11-20 14:51:16 -0300

    Find by id, exception error

--- backend/src/main/java/com/devsuperior/DesafioParaEntregar/resources/ClientResourse.java
+++ backend/src/main/java/com/devsuperior/DesafioParaEntregar/resources/ClientResourse.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +19,16 @@ public class ClientResourse {
 	@Autowired
 	private ClientService service;
 	
-	@GetMapping
+	@GetMapping											//SERVE PARA FAZER PESQUISA COMPLETA DO BANCO
 	public ResponseEntity<List<ClientDTO>> findAll() {
 		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}") 						//SERVE PARA FAZER PESQUISA POR ID DO BANCO
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) { //ANOTAÇÃO SERVE PARA CASAR COM A ANOTAÇÃO DO ID EM CIMA
+		ClientDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
