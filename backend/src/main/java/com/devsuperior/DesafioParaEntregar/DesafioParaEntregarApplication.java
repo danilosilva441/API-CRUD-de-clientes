83594582c6117e2e50623ef792cb300400f20a0b
Danilo da Silva Souza <danilosilva441@gmail.com>
2021-11-19 23:02:14 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/DesafioParaEntregar/DesafioParaEntregarApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DesafioParaEntregar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioParaEntregarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioParaEntregarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/DesafioParaEntregar/DesafioParaEntregarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DesafioParaEntregar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioParaEntregarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
