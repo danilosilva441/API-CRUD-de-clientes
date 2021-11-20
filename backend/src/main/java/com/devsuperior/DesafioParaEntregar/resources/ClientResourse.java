27466a761b5cbad5e00508ae5d8a8e534fcd3de3
Danilo da Silva Souza <danilosilva441@gmail.com>
2021-11-20 14:14:45 -0300

    DTO

--- backend/src/main/java/com/devsuperior/DesafioParaEntregar/resources/ClientResourse.java
+++ backend/src/main/java/com/devsuperior/DesafioParaEntregar/resources/ClientResourse.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.DesafioParaEntregar.entities.Client;
+import com.devsuperior.DesafioParaEntregar.dto.ClientDTO;
 import com.devsuperior.DesafioParaEntregar.services.ClientService;
 
 @RestController
@@ -19,8 +19,8 @@ public class ClientResourse {
 	private ClientService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = service.findAll();
+	public ResponseEntity<List<ClientDTO>> findAll() {
+		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
